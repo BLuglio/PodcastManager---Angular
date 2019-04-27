8abcac28adf73044e8c7b792c96e329dff943506
BLuglio <biagio.luglio@gmail.com>
2019-04-27 19:42:40 +0200

    podcast item - broken episodes list

--- src/app/podcast-item/podcast-item.component.ts
+++ src/app/podcast-item/podcast-item.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
 import { PodcastService } from '../podcast.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -14,9 +12,12 @@ export class PodcastItemComponent implements OnInit {
 
   public podcast;
   private id;
-  public data;
+  public episodesToShow = [];
+  public cursor;
+  public nextVisible: boolean;
+  public previousVisible: boolean;
 
-  constructor(private sanitizer: DomSanitizer, private route: ActivatedRoute, private podcastService: PodcastService) { }
+  constructor(private sanitizer: DomSanitizer, private route: ActivatedRoute, private podcastService: PodcastService) {}
 
   ngOnInit() {
     //take the podcast id from the path
@@ -26,22 +27,57 @@ export class PodcastItemComponent implements OnInit {
           switch(data.msg){
             case("cache"):
               this.podcast = data.data;
+              this.podcast.episodes = this.sanitizeContent(this.podcast.episodes);
               break;
             case("no-cache"):
               this.podcast = data.data.data.Rss;
+              this.podcast.episodes = this.sanitizeContent(this.podcast.episodes);
               //sanitizing the image url
               for(let item of this.podcast) {
                 item.imageUrl = this.sanitizer.bypassSecurityTrustResourceUrl(item.imageUrl);
               }
               break;
           }
+          this.cursor = 0;
+          this.previousVisible = false;
+          if(this.podcast.episodes.length > 10){
+            this.nextVisible = true;
+          }
+          for(let i = 0; i < 10; i++){
+            this.episodesToShow[i] = this.podcast.episodes[i];
+          } 
         }
-      })
-      console.log(this.podcast);
+      })       
   }
 
-  showPlayer() {
-    return this.podcast.content;
+  sanitizeContent(episodes) {
+    for(let item of episodes) {
+      item.content = this.sanitizer.bypassSecurityTrustResourceUrl(item.content);
+      item.description = item.description.replace(/<\/?[^>]+(>|$)/g, "");
+    }
+    return episodes;
   }
 
+  update(number){
+    this.cursor = number;
+    let tmp = [];
+    for(let i = 0; i < 10; i++){
+      if(this.podcast.episodes[(this.cursor * 10) + i]){
+        tmp[i] = this.podcast.episodes[(this.cursor * 10) + i];
+      }
+    }
+    if(this.cursor === Math.round(this.podcast.episodes.length/10)){
+      this.nextVisible = false;
+    }
+    if(this.cursor === 0){
+      this.previousVisible = false;
+    }
+    if(this.cursor > 0) {
+      this.previousVisible = true;
+    }
+    if(this.cursor < Math.round(this.podcast.episodes.length/10)){
+      this.nextVisible = true;
+    }
+    this.episodesToShow = tmp;
+  }
 }
